343fb562df39c408389ba55a48aa62f857353a81
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-11-10 21:16:30 -0300

    Estou criando um algoritmo que realiza a soma de dois numeros

---
+++ add.js
@@ -0,0 +1,23 @@
+// Somar dois numeros este é o meu problema
+
+
+// pra isto vou  utilizar o meu algoritmo 
+//Como resolver meu problema com o meu algoritmo
+
+// ideias  pseudocodigo  ou uma linguagem de programação
+
+// javascript
+
+// variaveis 
+
+const number1 = 10;
+
+const number2 = 4
+
+
+
+
+const soma = number1+number2;
+const string = 'c';
+ console.log(number1+number2)
+//console.log(soma)

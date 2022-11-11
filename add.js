d897b96bdd8c81c1f482bbda719290c9006f6415
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-11-10 21:45:10 -0300

    Atualizei meu software para uma soma com função

--- add.js
+++ add.js
@@ -12,12 +12,15 @@
 
 const number1 = 10;
 
-const number2 = 4
+const number2 = 4;
 
+function Soma(numero1,numero2){
+    return numero1+numero2;
+}
 
+console.log(Soma(20,30))
 
-
-const soma = number1+number2;
-const string = 'c';
+//const soma = number1+number2;
+//const string = 'c';
  console.log(number1+number2)
 //console.log(soma)
